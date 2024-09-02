3b3812613369d646bc91614380c002ac3e0b9036
zaynabmamaa@gmail.com <zaynabmamaa@gmail.com>
2024-09-03 02:04:41 +0300

    added type annotations to Button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,9 +1,18 @@
-const Button = () => {
-    return (
-        <div>
-            Enter
-        </div>
-    );
+import React from 'react';
+import styles from '../styles/Button.module.css';
+
+interface ButtonProps {
+  type?: 'button' | 'submit' | 'reset';
+  onClick?: () => void;
+  children: React.ReactNode;
 }
+
+const Button = ({ type = 'button', onClick, children }: ButtonProps) => {
+  return (
+    <button type={type} onClick={onClick} className={styles.button}>
+      {children}
+    </button>
+  );
+};
 
 export default Button;
