f8e9306036a09e89200b6a13d8cad552fc156808
zaynabmamaa@gmail.com <zaynabmamaa@gmail.com>
2024-09-04 01:06:08 +0300

    fetching api for sign up

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,77 @@
+'use client';
+
+import React, { useState } from 'react';
+import Input from '@/components/Input';
+import Button from '@/components/Button';
+import styles from '../../styles/Signup.module.css';
+import { useRouter } from 'next/navigation';
+
+const SignUp: React.FC = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [submissionError, setSubmissionError] = useState(''); // For post-submission error
+  const router = useRouter();
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    const signUpData = { name, email, password };
+
+    try {
+      const response = await fetch('http://localhost:5000/auth/signup', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(signUpData),
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        console.log('Sign up successful:', data);
+        router.push('/login'); 
+      } else {
+        const errorData = await response.json();
+        console.error('Sign up failed:', errorData.message);
+        setSubmissionError(errorData.message); 
+      }
+    } catch (error) {
+      console.error('Error during sign-up:', error);
+      setSubmissionError('An unexpected error occurred. Please try again later.');
+    }
+  };
+
+  return (
+    <div className={styles.container}>
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <h2 className={styles.title}>Create an account</h2>
+        <Input
+          type="text"
+          placeholder="Enter your name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <Input
+          type="email"
+          placeholder="Enter your email address"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <Input
+          type="password"
+          placeholder="Enter your password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <Button type="submit">Sign up</Button>
+      </form>
+      {submissionError && <div className={styles.error}>{submissionError}</div>}
+      <p className={styles.footerText}>
+        Already have an account? <a href="/login">Log in</a>
+      </p>
+    </div>
+  );
+};
+
+export default SignUp;
