36ae300650ea46debf6ce551b8e7a2c90eaf5dca
zaynabmamaa@gmail.com <zaynabmamaa@gmail.com>
2024-09-08 20:37:17 +0300

    Adding button for users

--- src/components/prPredefinedComponents.tsx
+++ src/components/prPredefinedComponents.tsx
@@ -5,6 +5,17 @@ interface Component {
   }
   
   export const availableComponents: Component[] = [
+    {
+      type: 'button',
+      label: 'Button',
+      defaultProperties: {
+        text: 'Click Me',
+        backgroundColor: 'blue',
+        color: 'red',
+        fontSize: '16px',
+      },
+    },
+    
    
   ];
   
