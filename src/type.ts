358a7cb43cfb9a9989748e9d083dfdf737461166
zaynabmamaa@gmail.com <zaynabmamaa@gmail.com>
2024-09-14 16:13:54 +0300

    editing type file

--- src/type.ts
+++ src/type.ts
@@ -1,11 +1,32 @@
+// export interface ComponentItem {
+//   _id?: string;
+//   type: string;
+//   label:string;
+//   properties: Record<string, any>;
+//   children?: ComponentItem[];
+// }
+// import React from 'react';
+
 export interface ComponentItem {
   _id?: string;
   type: string;
-  properties: Record<string, any>;
+  properties: React.CSSProperties; 
+  label?: string;
 }
 
 export interface Page {
   _id: string;
   name: string;
   components: ComponentItem[];
+}
+
+// export interface ComponentProps {
+//   properties?: Record<string, any>;
+// }
+
+
+export interface Project {
+  _id: string;
+  name: string;
+  pages: Page[];
 }
