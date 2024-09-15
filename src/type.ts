a27af57e665cdfbe41871562336c291ea2d0720d
zaynabmamaa@gmail.com <zaynabmamaa@gmail.com>
2024-09-15 20:30:00 +0300

    redefining properties

--- src/type.ts
+++ src/type.ts
@@ -1,25 +1,35 @@
 
-export interface ComponentItem {
-  _id?: string;
-  type: string;
-  properties: React.CSSProperties;
-  text?: string;
-  children?: ComponentItem[]; 
-}
-
-export interface Page {
-  _id: string;
-  name: string;
-  components: ComponentItem[];
-}
-
-// export interface ComponentProps {
-//   properties?: Record<string, any>;
+// export interface ComponentItem {
+//   _id?: string;
+//   type: string;
+//   properties: React.CSSProperties;
+//   text?: string;
+//   children?: ComponentItem[]; 
 // }
 
+ export interface Page {
+   _id: string;
+   name: string;
+   components: ComponentItem[];
+}
 
-export interface Project {
-  _id: string;
-  name: string;
-  pages: Page[];
+// // export interface ComponentProps {
+// //   properties?: Record<string, any>;
+// // }
+
+
+// export interface Project {
+//   _id: string;
+//   name: string;
+//   pages: Page[];
+// }
+interface ComponentProperties {
+  [key: string]: any; 
+}
+
+export interface ComponentItem {
+  type: string;
+  properties: ComponentProperties; 
+
+  _id?: string;
 }
